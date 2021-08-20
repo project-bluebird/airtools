0c0a059e331878091ae65fecfdc6bfac79b39b19
Radka Jersakova <r.jersakova@gmail.com>
2021-08-20 09:23:45 +0100

    create package dir

---
+++ airtools/__version__.py
@@ -0,0 +1,7 @@
+# airtools package version. When merging changes to master:
+# - increment 2nd digit for new features
+# - increment 3rd digit for bug fixes
+
+VERSION = (0, 1, 0)
+
+__version__ = ".".join(map(str, VERSION))
